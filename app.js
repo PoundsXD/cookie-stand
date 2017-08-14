c87de72de7b73155e6a8a526b00917e9e355f8c9
Brian Wheeler <monsteraono@gmail.com>
2017-08-14 15:31:41 -0700

    Made initial sales object for firstPike.

--- app.js
+++ app.js
@@ -4,8 +4,13 @@ var firstPike = {
   minCust:23,
   maxCust:65,
   avgCookie:6.3,
+  cookiesSold:[],
   cookieSales: function() {
-    var sales = Math.floor(Math.random() * (this.maxCust - this.minCust) + this.minCust);
-    console.log(sales);
+    for (var i = 0; i < 15; i++) {
+      var sales = Math.floor(Math.random(this.avgCookie) * (this.maxCust - this.minCust) + this.minCust);
+      console.log(sales);
+      this.cookiesSold.push[sales];
+    }
   }
 };
+firstPike.cookieSales();
