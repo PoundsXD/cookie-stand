a774b43b657ac158c8fdb4adca4bedb52b4882d3
Brian Wheeler <monsteraono@gmail.com>
2017-08-14 21:52:28 -0700

    Added remaining stores to app.js page.

--- app.js
+++ app.js
@@ -42,7 +42,7 @@ var seaTac = {
   avgCookie:1.2,
   cookiesSold2:[],
   dailyCookieSales2: 0,
-  cookieSales: function() {
+  cookieSales2: function() {
     for (var i = 0; i < 15; i++) {
       var sales = Math.floor(Math.random() * (this.maxCust - this.minCust) + this.minCust);
       var totalSales = Math.floor(sales * this.avgCookie);
@@ -51,7 +51,7 @@ var seaTac = {
     }
   }
 };
-firstPike.cookieSales();
+seaTac.cookieSales2();
 document.getElementById('store2item1').innerHTML = seaTac.cookiesSold2[0];
 document.getElementById('store2item1').innerHTML = seaTac.cookiesSold2[1];
 document.getElementById('store2item3').innerHTML = seaTac.cookiesSold2[2];
@@ -70,3 +70,107 @@ document.getElementById('store2item15').innerHTML = seaTac.cookiesSold2[14];
 document.getElementById('store2item16').innerHTML = seaTac.dailyCookieSales2;
 document.getElementById('store2item17').innerHTML = seaTac.storeName;
 //Seattle Center
+var seacenter = {
+  storeName: 'Seattle Center.',
+  minCust:11,
+  maxCust:38,
+  avgCookie:3.7,
+  cookiesSold3:[],
+  dailyCookieSales3: 0,
+  cookieSales3: function() {
+    for (var i = 0; i < 15; i++) {
+      var sales = Math.floor(Math.random() * (this.maxCust - this.minCust) + this.minCust);
+      var totalSales = Math.floor(sales * this.avgCookie);
+      this.dailyCookieSales3 = totalSales + this.dailyCookieSales3;
+      this.cookiesSold3.push(totalSales);
+    }
+  }
+};
+seacenter.cookieSales3();
+document.getElementById('store3item1').innerHTML = seacenter.cookiesSold[0];
+document.getElementById('store3item2').innerHTML = seacenter.cookiesSold[1];
+document.getElementById('store3item3').innerHTML = seacenter.cookiesSold[2];
+document.getElementById('store3item4').innerHTML = seacenter.cookiesSold[3];
+document.getElementById('store3item5').innerHTML = seacenter.cookiesSold[4];
+document.getElementById('store3item6').innerHTML = seacenter.cookiesSold[5];
+document.getElementById('store3item7').innerHTML = seacenter.cookiesSold[6];
+document.getElementById('store3item8').innerHTML = seacenter.cookiesSold[7];
+document.getElementById('store3item9').innerHTML = seacenter.cookiesSold[8];
+document.getElementById('store3item10').innerHTML = seacenter.cookiesSold[9];
+document.getElementById('store3item11').innerHTML = seacenter.cookiesSold[10];
+document.getElementById('store3item12').innerHTML = seacenter.cookiesSold[11];
+document.getElementById('store3item13').innerHTML = seacenter.cookiesSold[12];
+document.getElementById('store3item14').innerHTML = seacenter.cookiesSold[13];
+document.getElementById('store3item15').innerHTML = seacenter.cookiesSold[14];
+document.getElementById('store3item16').innerHTML = seacenter.dailyCookieSales3;
+document.getElementById('store3item17').innerHTML = seacenter.storeName;
+//Capitol Hill
+var capitolhill = {
+  storeName: 'Capitol Hill.',
+  minCust:20,
+  maxCust:38,
+  avgCookie:2.3,
+  cookiesSold4:[],
+  dailyCookieSales4: 0,
+  cookieSales4: function() {
+    for (var i = 0; i < 15; i++) {
+      var sales = Math.floor(Math.random() * (this.maxCust - this.minCust) + this.minCust);
+      var totalSales = Math.floor(sales * this.avgCookie);
+      this.dailyCookieSales4 = totalSales + this.dailyCookieSales4;
+      this.cookiesSold4.push(totalSales);
+    }
+  }
+};
+capitolhill.cookieSales4();
+document.getElementById('store4item1').innerHTML = capitolhill.cookiesSold[0];
+document.getElementById('store4item2').innerHTML = capitolhill.cookiesSold[1];
+document.getElementById('store4item3').innerHTML = capitolhill.cookiesSold[2];
+document.getElementById('store4item4').innerHTML = capitolhill.cookiesSold[3];
+document.getElementById('store4item5').innerHTML = capitolhill.cookiesSold[4];
+document.getElementById('store4item6').innerHTML = capitolhill.cookiesSold[5];
+document.getElementById('store4item7').innerHTML = capitolhill.cookiesSold[6];
+document.getElementById('store4item8').innerHTML = capitolhill.cookiesSold[7];
+document.getElementById('store4item9').innerHTML = capitolhill.cookiesSold[8];
+document.getElementById('store4item10').innerHTML = capitolhill.cookiesSold[9];
+document.getElementById('store4item11').innerHTML = capitolhill.cookiesSold[10];
+document.getElementById('store4item12').innerHTML = capitolhill.cookiesSold[11];
+document.getElementById('store4item13').innerHTML = capitolhill.cookiesSold[12];
+document.getElementById('store4item14').innerHTML = capitolhill.cookiesSold[13];
+document.getElementById('store4item15').innerHTML = capitolhill.cookiesSold[14];
+document.getElementById('store4item16').innerHTML = capitolhill.dailyCookieSales4;
+document.getElementById('store4item17').innerHTML = capitolhill.storeName;
+//Alki
+var alk = {
+  storeName: 'Alki.',
+  minCust:2,
+  maxCust:16,
+  avgCookie:4.6,
+  cookiesSold5:[],
+  dailyCookieSales5: 0,
+  cookieSales5: function() {
+    for (var i = 0; i < 15; i++) {
+      var sales = Math.floor(Math.random() * (this.maxCust - this.minCust) + this.minCust);
+      var totalSales = Math.floor(sales * this.avgCookie);
+      this.dailyCookieSales5 = totalSales + this.dailyCookieSales5;
+      this.cookiesSold5.push(totalSales);
+    }
+  }
+};
+alk.cookieSales5();
+document.getElementById('store5item1').innerHTML = alk.cookiesSold[0];
+document.getElementById('store5item2').innerHTML = alk.cookiesSold[1];
+document.getElementById('store5item3').innerHTML = alk.cookiesSold[2];
+document.getElementById('store5item4').innerHTML = alk.cookiesSold[3];
+document.getElementById('store5item5').innerHTML = alk.cookiesSold[4];
+document.getElementById('store5item6').innerHTML = alk.cookiesSold[5];
+document.getElementById('store5item7').innerHTML = alk.cookiesSold[6];
+document.getElementById('store5item8').innerHTML = alk.cookiesSold[7];
+document.getElementById('store5item9').innerHTML = alk.cookiesSold[8];
+document.getElementById('store5item10').innerHTML = alk.cookiesSold[9];
+document.getElementById('store5item11').innerHTML = alk.cookiesSold[10];
+document.getElementById('store5item12').innerHTML = alk.cookiesSold[11];
+document.getElementById('store5item13').innerHTML = alk.cookiesSold[12];
+document.getElementById('store5item14').innerHTML = alk.cookiesSold[13];
+document.getElementById('store5item15').innerHTML = alk.cookiesSold[14];
+document.getElementById('store5item16').innerHTML = alk.dailyCookieSales5;
+document.getElementById('store5item17').innerHTML = alk.storeName;
