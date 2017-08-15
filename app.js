f2add4b065f3aec9c6e26b2ce049aa6886275931
Brian Wheeler <monsteraono@gmail.com>
2017-08-14 17:28:51 -0700

    Fixed first stores function.

--- app.js
+++ app.js
@@ -5,12 +5,17 @@ var firstPike = {
   maxCust:65,
   avgCookie:6.3,
   cookiesSold:[],
+  dailyCookieSales: 0,
   cookieSales: function() {
     for (var i = 0; i < 15; i++) {
-      var sales = Math.floor(Math.random(this.avgCookie) * (this.maxCust - this.minCust) + this.minCust);
-      console.log(sales);
-      this.cookiesSold.push[sales];
+      var sales = Math.floor(Math.random() * (this.maxCust - this.minCust) + this.minCust);
+      var totalSales = Math.floor(sales * this.avgCookie);
+      this.dailyCookieSales = totalSales + this.dailyCookieSales;
+      console.log(totalSales);
+      this.cookiesSold.push[totalSales];
     }
   }
 };
 firstPike.cookieSales();
+console.log(firstPike.dailyCookieSales);
+document.getElementById('item1').innerHTML = firstPike.cookiesSold[0];
